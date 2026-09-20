5c8b91a38de10d1aac0a8fcf3b2e8287fa14b646
priyanshisrivastava-marketer <priyanshi18uwsb@gmail.com>
2026-09-20 16:58:22 +0530

    Disable stale service worker caching while fixing mobile navigation

--- app/register-sw.js
+++ app/register-sw.js
@@ -4,8 +4,12 @@ import { useEffect } from "react";
 
 export default function RegisterSW() {
   useEffect(() => {
-    if ("serviceWorker" in navigator) {
-      navigator.serviceWorker.register("/sw.js").catch((e) => console.error("SW registration failed", e));
+    if (!("serviceWorker" in navigator)) return;
+    navigator.serviceWorker.getRegistrations().then((registrations) => {
+      registrations.forEach((registration) => registration.unregister());
+    }).catch(() => {});
+    if ("caches" in window) {
+      caches.keys().then((keys) => keys.forEach((key) => caches.delete(key))).catch(() => {});
     }
   }, []);
   return null;
